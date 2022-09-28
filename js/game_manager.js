a5e17ec5ad19aaf23bcfb43d04a4d88ce4579b63
parmeseancheez <68348181+parmeseancheez@users.noreply.github.com>
2022-09-28 10:53:15 -0500

    Update game_manager.js
    Random 1024’s now

--- js/game_manager.js
+++ js/game_manager.js
@@ -69,8 +69,8 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    //var value = Math.random() < 0.9 ? 2 : 4;
-	 var value = 2048;
+    var value = Math.random() < 0.9 ? 2048 : 1024;
+    //var value = 2048;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
